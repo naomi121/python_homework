8db488d29618ad8fe960accb8f4cca0f16de7546
Naomi Rodriguez <naomi.rodriguez7@aol.com>
2026-07-07 16:43:51 -0400

    adding Task 5

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -41,3 +41,41 @@ def calc(val1, val2, operation="multiply"):
     except TypeError:
         return "You can't multiply those values!"
 
+
+#Task 4: Data Type Conversion
+
+def data_type_conversion(value, type_requested):
+    try:
+        match type_requested:
+            case "float":
+                return float(value)
+            case "str":
+                return str(value)
+            case "int":
+                return int(value)
+            case _:
+                return "Unknown type requested"
+    except ValueError:
+        return f"You can't convert {value} into a {type_requested}."
+    
+
+
+    #Task 5: Grade
+
+    def grade(*args):
+        try:
+            average = sum(args) / len(args)
+            
+            if average >= 90:
+                return "A"
+            elif average >= 80:
+                return "B"
+            elif average >= 70:
+                return "C"
+            elif average >= 60:
+                return "D"
+            else:
+                return "F"
+                
+        except Exception:
+         return "Invalid data was provided."
