195bebce02af7b2ad6195dd9bdd165d63fd8415a
Naomi Rodriguez <naomi.rodriguez7@aol.com>
2026-07-07 16:23:07 -0400

    Task 2

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -12,4 +12,33 @@ def hello():
 #Task 2:
 
 def name():
-    return "greet"
+    return 'Hello, Name!'
+
+
+
+#Task 3: Calculator 
+
+def calc(val1, val2, operation="multiply"):
+    try:
+        match operation:
+            case "add":
+                return val1 + val2
+            case "subtract":
+                return val1 - val2
+            case "multiply":
+                return val1 * val2
+            case "divide":
+                return val1 / val2
+            case "modulo":
+                return val1 % val2
+            case "int_divide":
+                return val1 // val2
+            case "power":
+                return val1 ** val2
+            case _:
+                return "Unknown operation"
+    except ZeroDivisionError:
+        return "You can't divide by 0!"
+    except TypeError:
+        return "You can't multiply those values!"
+
