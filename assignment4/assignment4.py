252f6d5882591f61ac4b9a9513da7902515fb229
Naomi Rodriguez <naomi.rodriguez7@aol.com>
2026-07-29 17:36:24 -0400

    fixing task 4

--- assignment4/assignment4.py
+++ assignment4/assignment4.py
@@ -55,18 +55,15 @@ print(dirty_data)
 
 clean_data = dirty_data.copy()
 
-clean_data = clean_data.drop_duplicates()
+clean_data.drop_duplicates(inplace=True)
 
 clean_data['Age'] = pd.to_numeric(clean_data['Age'], errors='coerce')
 
 clean_data['Salary'] = clean_data['Salary'].replace(['unknown', 'n/a'], pd.NA)
 clean_data['Salary'] = pd.to_numeric(clean_data['Salary'], errors='coerce')
 
-mean_age = clean_data['Age'].mean()
-median_salary = clean_data['Salary'].median()
-
-clean_data['Age'] = clean_data['Age'].fillna(mean_age)
-clean_data['Salary'] = clean_data['Salary'].fillna(median_salary)
+clean_data['Age'] = clean_data['Age'].fillna(clean_data['Age'].mean())
+clean_data['Salary'] = clean_data['Salary'].fillna(clean_data['Salary'].median())
 
 clean_data['Hire Date'] = pd.to_datetime(clean_data['Hire Date'], errors='coerce')
 
